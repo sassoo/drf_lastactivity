1b0aa04fb7b557d8183b7065cb7c5f99a0a3e853
sassoo <scottmcclanahan2002@gmail.com>
2017-06-23 13:34:18 +0000

    broke out logic into different methods for easier subclassing

--- drf_lastactivity/middlewares.py
+++ drf_lastactivity/middlewares.py
@@ -21,16 +21,29 @@ class UpdateLastActivityMiddleware:
     By default 5 minutes.
     """
 
+    LAST_LOGIN_FIELD = 'last_login'
     SPLAY_TIME = 300
+
+    def get_authenticated_user(self, request):
+        """ Return a Django user model object """
+
+        user = getattr(request, 'user', None)
+        if user and user.is_authenticated():
+            return user
 
     def process_response(self, request, response):
         """ Django override, return the response """
 
-        user = getattr(request, 'user', None)
-        if user and user.is_authenticated():
-            now = timezone.now()
-            last = user.last_login
-            if not last or (now - last).seconds > self.SPLAY_TIME:
-                user.__class__.objects.filter(pk=user.pk) \
-                    .update(last_login=now)
+        user = self.get_authenticated_user(request)
+        if user:
+            self.update_last_login(user)
         return response
+
+    def update_last_login(self, user):
+        """ Update the user models LAST_LOGIN_FIELD """
+
+        now = timezone.now()
+        last = getattr(user, self.LAST_LOGIN_FIELD, None)
+        if not last or (now - last).seconds > self.SPLAY_TIME:
+            attrs = {self.LAST_LOGIN_FIELD: now}
+            user.__class__.objects.filter(pk=user.pk).update(**attrs)
